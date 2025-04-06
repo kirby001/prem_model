1439dd6518b423bab67c71349b68b7981c08b0f1
Luke <dietcoke157@gmail.com>
2025-04-06 18:25:39 +0100

    added Poisson regression improvement

--- prem_model.py
+++ prem_model.py
@@ -2,8 +2,25 @@ from understatapi import UnderstatClient
 import pandas as pd
 from scipy.stats import poisson
 import multiprocessing
+import datetime as dt
+import numpy as np
+import statsmodels.api as sm
 
 FIXTURES_URL = 'https://fixturedownload.com/feed/json/epl-2024'
+
+SEASONS = [
+    '2014',
+    '2015',
+    '2016',
+    '2017',
+    '2018',
+    '2019',
+    '2020',
+    '2021',
+    '2022',
+    '2023',
+    '2024'
+]
 
 
 def create_fixtures_teams(url):
@@ -76,16 +93,74 @@ def create_xG_table(league="EPL", season="2024"):
         team_data['away_xGA_per_90'] = away_xGA / away_played
         all_teams_data.append(team_data)
     xG_df = pd.DataFrame.from_records(all_teams_data)
-    xG_df['home_multiplier'] = xG_df['home_xGA_per_90'] / xG_df['home_xGA_per_90'].mean()
-    xG_df['away_multiplier'] = xG_df['away_xGA_per_90'] / xG_df['away_xGA_per_90'].mean()
     return xG_df
 
 
-def simulate_league(table, fixtures, xgdf):
+def create_xG_table(league="EPL", season="2024"):
+    understat = UnderstatClient()
+    league_team_data = understat.league(league=league).get_team_data(season=season)
+    all_teams_data = []
+    for key in league_team_data.keys():
+        team_data = {}
+        team_name = league_team_data[key]['title']
+        home_xG = 0.0
+        home_xGA = 0.0
+        home_played = 0
+        away_xG = 0.0
+        away_xGA = 0.0
+        away_played = 0
+        team_data['name'] = team_name
+        for match in league_team_data[key]['history']:
+            if match['h_a'] == 'h':
+                home_xG += match['xG']
+                home_xGA += match['xGA']
+                home_played += 1
+            elif match['h_a'] == 'a':
+                away_xG += match['xG']
+                away_xGA += match['xGA']
+                away_played += 1
+        team_data['home_xG_per_90'] = home_xG / home_played
+        team_data['home_xGA_per_90'] = home_xGA / home_played
+        team_data['away_xG_per_90'] = away_xG / away_played
+        team_data['away_xGA_per_90'] = away_xGA / away_played
+        all_teams_data.append(team_data)
+    xG_df = pd.DataFrame.from_records(all_teams_data)
+    return xG_df
+
+
+def match_xG_data(xgdf, league='EPL', season='2024'):
+    understat = UnderstatClient()
+    league_match_data = understat.league(league=league).get_match_data(season=season)
+    all_matches_data = []
+    for match in league_match_data:
+        if match['isResult']:
+            match_data = {}
+            match_data['home_team'] = match['h']['title']
+            match_data['away_team'] = match['a']['title']
+            match_data['home_goals'] = int(match['goals']['h'])
+            match_data['away_goals'] = int(match['goals']['a'])
+            match_data['home_xG'] = xgdf[xgdf['name'] == match_data['home_team']].iloc[0]['home_xG_per_90']
+            match_data['away_xGA'] = xgdf[xgdf['name'] == match_data['away_team']].iloc[0]['away_xGA_per_90']
+            match_data['away_xG'] = xgdf[xgdf['name'] == match_data['away_team']].iloc[0]['away_xG_per_90']
+            match_data['home_xGA'] = xgdf[xgdf['name'] == match_data['home_team']].iloc[0]['home_xGA_per_90']
+            all_matches_data.append(match_data)
+    match_xG_df = pd.DataFrame.from_records(all_matches_data)
+    return match_xG_df
+
+
+def predict_home_goals(home_xG, away_xGA, params):
+    return np.exp(params[0] + params[1] * home_xG + params[2] * away_xGA)
+
+
+def predict_away_goals(away_xG, home_xGA, params):
+    return np.exp(params[0] + params[1] * away_xG + params[2] * home_xGA)
+
+
+def simulate_league(table, fixtures, xgdf, home_params, away_params):
     table = table.copy()
     for fixture in fixtures:
-        home_mu = xgdf[xgdf['name'] == fixture['HomeTeam']].iloc[0]['home_xG_per_90'] * xgdf[xgdf['name'] == fixture['AwayTeam']].iloc[0]['away_multiplier']
-        away_mu = xgdf[xgdf['name'] == fixture['AwayTeam']].iloc[0]['away_xG_per_90'] * xgdf[xgdf['name'] == fixture['HomeTeam']].iloc[0]['home_multiplier']
+        home_mu = predict_home_goals(xgdf[xgdf['name'] == fixture['HomeTeam']].iloc[0]['home_xG_per_90'], xgdf[xgdf['name'] == fixture['AwayTeam']].iloc[0]['away_xGA_per_90'], home_params)
+        away_mu = predict_away_goals(xgdf[xgdf['name'] == fixture['AwayTeam']].iloc[0]['away_xG_per_90'], xgdf[xgdf['name'] == fixture['HomeTeam']].iloc[0]['home_xGA_per_90'], away_params)
         home_goals = poisson.rvs(home_mu)
         away_goals = poisson.rvs(away_mu)
         table.loc[table['name'] == fixture['HomeTeam'], 'played'] += 1
@@ -131,20 +206,51 @@ def simulate_league(table, fixtures, xgdf):
     return outcome
 
 
-def run_multiple_simulations(number_of_simulations, current_table, fixtures, xgdf):
+def run_multiple_simulations(number_of_simulations: int, current_table: pd.DataFrame, fixtures: list, xgdf: pd.DataFrame, home_params: list, away_params: list):
     num_cores = multiprocessing.cpu_count()
     pool = multiprocessing.Pool(num_cores)
-    results = pool.starmap(simulate_league, [(current_table, fixtures, xgdf) for _ in range(number_of_simulations)])
+    results = pool.starmap(simulate_league, [(current_table, fixtures, xgdf, home_params, away_params) for _ in range(number_of_simulations)])
     pool.close()
     pool.join()
     return results
 
 
+def current_date_for_file():
+    date = dt.datetime.now()
+    year = date.year
+    month = date.month
+    day = date.day
+    hour = date.hour
+    minute = date.minute
+    second = date.second
+    return f"{year}-{month:02d}-{day:02d}T{hour:02d}_{minute:02d}_{second:02d}"
+
+
 if __name__ == '__main__':
+    print(dt.datetime.now())
     all_teams, completed_fixtures, upcoming_fixtures = create_fixtures_teams(FIXTURES_URL)
     team_records = [create_team_record(team=team, completed_fixtures=completed_fixtures) for team in all_teams]
     current_league_table = create_league_table(team_records)
     xG_df = create_xG_table()
-    outcomes = run_multiple_simulations(200, current_league_table, upcoming_fixtures, xG_df)
+    dfs = []
+    for season in SEASONS:
+        xgdf = create_xG_table(season=season)
+        matchxgdf = match_xG_data(xgdf=xgdf, season=season)
+        dfs.append(matchxgdf)
+    all_matches_df = pd.concat(dfs, ignore_index=True)
+    df_home = all_matches_df[['home_xG', 'away_xGA', 'home_goals']]
+    df_away = all_matches_df[['away_xG', 'home_xGA', 'away_goals']]
+    X_home = df_home[['home_xG', 'away_xGA']]
+    X_home = sm.add_constant(X_home)  # Add intercept
+    y_home = df_home['home_goals']
+    poisson_home = sm.GLM(y_home, X_home, family=sm.families.Poisson()).fit()
+    home_params = [poisson_home.params['const'], poisson_home.params['home_xG'], poisson_home.params['away_xGA']]
+    X_away = df_away[['away_xG', 'home_xGA']]
+    X_away = sm.add_constant(X_away)  # Add intercept
+    y_away = df_away['away_goals']
+    poisson_away = sm.GLM(y_away, X_away, family=sm.families.Poisson()).fit()
+    away_params = [poisson_away.params['const'], poisson_away.params['away_xG'], poisson_away.params['home_xGA']]
+    outcomes = run_multiple_simulations(100000, current_league_table, upcoming_fixtures, xG_df, home_params, away_params)
     outcome_df = pd.DataFrame.from_records(outcomes)
-    outcome_df.to_csv('outcome2.csv')
+    outcome_df.to_csv(f'outcome_{current_date_for_file()}.csv')
+    print(dt.datetime.now())
